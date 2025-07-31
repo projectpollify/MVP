b17850696523f654857bab53586d135626033853
Shawn Breau <shawn@pollify.net>
2025-07-31 15:36:02 -0700

    Create index.ts

---
+++ src/modules/auth/index.ts
@@ -0,0 +1,5 @@
+// Export all auth module components
+export * from './auth.controller';
+export * from './auth.service';
+export * from './auth.middleware';
+export { default as authRoutes } from './auth.routes';
